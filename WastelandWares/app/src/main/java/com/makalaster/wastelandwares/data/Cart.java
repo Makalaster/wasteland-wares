5129079363dd4cfb40ac4c6f00297e0af3f72290
Sam Perlow <perlow.sam@gmail.com>
2017-04-11 15:48:10 -0400

    Converted cart object to singleton for persistent access across app

--- WastelandWares/app/src/main/java/com/makalaster/wastelandwares/data/Cart.java
+++ WastelandWares/app/src/main/java/com/makalaster/wastelandwares/data/Cart.java
@@ -8,17 +8,26 @@ import java.util.HashMap;
  */
 
 public class Cart {
-    private HashMap<Long, Integer> mContents;
+    private HashMap<ItemId, Integer> mContents;
     private Date mCartCompletionDate;
     private double mTotal;
 
-    public Cart(Date cartCompletionDate, double total) {
-        mCartCompletionDate = cartCompletionDate;
-        mTotal = total;
+    private static Cart sCart;
+
+    public Cart() {
+        mCartCompletionDate = new Date();
+        mTotal = 0.0;
         mContents = new HashMap<>();
     }
 
-    public HashMap<Long, Integer> getContents() {
+    public static Cart getInstance() {
+        if (sCart == null) {
+            sCart = new Cart();
+        }
+        return sCart;
+    }
+
+    public HashMap<ItemId, Integer> getContents() {
         return mContents;
     }
 
@@ -38,7 +47,45 @@ public class Cart {
         return mCartCompletionDate;
     }
 
+    public void incrementItemCount(ItemId id) {
+        mContents.put(id, mContents.get(id) + 1);
+    }
+
+    public void decrementItemCount(ItemId id) {
+        mContents.put(id, mContents.get(id) - 1);
+    }
+
+    public void addItemToCart(ItemId id) {
+        if (mContents.containsKey(id)) {
+            mContents.put(id, mContents.get(id) + 1);
+        } else {
+            mContents.put(id, 1);
+        }
+    }
+
+    public void removeItemFromCart(ItemId id) {
+        mContents.remove(id);
+    }
+
     public double getTotal() {
         return mTotal;
+    }
+
+    public class ItemId {
+        private Long mItemId;
+        private String mItemType;
+
+        public ItemId(long id, String type) {
+            mItemId = id;
+            mItemType = type;
+        }
+
+        public Long getItemId() {
+            return mItemId;
+        }
+
+        public String getItemType() {
+            return mItemType;
+        }
     }
 }
