899cdb67aaad71739bc0d802a1a0b90a96ddba0d
Makalaster <maka.laster@gmail.com>
2017-04-13 18:09:12 -0400

    Completed test class for Aid pojo

--- WastelandWares/app/src/test/java/com/makalaster/wastelandwares/AidTest.java
+++ WastelandWares/app/src/test/java/com/makalaster/wastelandwares/AidTest.java
@@ -1,8 +1,37 @@
 package com.makalaster.wastelandwares;
 
+import com.makalaster.wastelandwares.data.Aid;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Makalaster on 4/13/17.
  */
 
 public class AidTest {
+    private Aid mTestAid;
+
+    @Before
+    public void setup() {
+        mTestAid = new Aid("Test Aid", "Aid made for testing", 5.0, 4.0, 123, 1, 5, 1);
+    }
+
+    @Test
+    public void testGetHp() {
+        int expectedHp = 5;
+        int actualHp = mTestAid.getHp();
+
+        assertEquals(expectedHp, actualHp);
+    }
+
+    @Test
+    public void testGetRads() {
+        int expectedRads = 1;
+        int actualRads = mTestAid.getRads();
+
+        assertEquals(expectedRads, actualRads);
+    }
 }
--- WastelandWares/app/src/test/java/com/makalaster/wastelandwares/ArmorTest.java
+++ WastelandWares/app/src/test/java/com/makalaster/wastelandwares/ArmorTest.java
@@ -12,17 +12,17 @@ import static org.junit.Assert.assertEquals;
  */
 
 public class ArmorTest {
-    private Armor mArmor;
+    private Armor mTestArmor;
 
     @Before
     public void setup() {
-        mArmor = new Armor("Test Armor", "An armor made for testing", 100.0, 3.0, 123, 100, 100, 50);
+        mTestArmor = new Armor("Test Armor", "An armor made for testing", 100.0, 3.0, 123, 100, 100, 50);
     }
 
     @Test
     public void testGetDefense() {
         int expectedDefense = 50;
-        int actualDefense = mArmor.getDefense();
+        int actualDefense = mTestArmor.getDefense();
 
         assertEquals(expectedDefense, actualDefense);
     }
