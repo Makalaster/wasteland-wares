1b325be362f6ed40c0fc592ce77d17fc67ed26df
Makalaster <maka.laster@gmail.com>
2017-04-13 18:21:49 -0400

    Completed test class for ItemId pojo

--- WastelandWares/app/src/test/java/com/makalaster/wastelandwares/ItemIdTest.java
+++ WastelandWares/app/src/test/java/com/makalaster/wastelandwares/ItemIdTest.java
@@ -1,8 +1,38 @@
 package com.makalaster.wastelandwares;
 
+import com.makalaster.wastelandwares.data.ItemId;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Makalaster on 4/13/17.
  */
 
 public class ItemIdTest {
+    private ItemId mTestItemId;
+
+    @Before
+    public void setup() {
+        mTestItemId = new ItemId(123, "Test type");
+    }
+
+    @Test
+    public void testGetItemId() {
+        long expectedItemId = 123;
+        long actualItemId = mTestItemId.getItemId();
+
+        assertEquals(expectedItemId, actualItemId);
+    }
+
+    @Test
+    public void testGetItemType() {
+        String expectedType = "Test type";
+        String actualType = mTestItemId.getItemType();
+
+        assertEquals(expectedType, actualType);
+    }
+
 }
