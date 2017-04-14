007ec8c95ea2bf0f1491d7b3fe17db6ba6df4cc4
Sam Perlow <perlow.sam@gmail.com>
2017-04-14 14:00:13 -0400

    Updated readme

--- WastelandWares/app/src/androidTest/java/com/makalaster/wastelandwares/shopWares/CartActivityTest.java
+++ WastelandWares/app/src/androidTest/java/com/makalaster/wastelandwares/shopWares/CartActivityTest.java
@@ -1,24 +1,16 @@
 package com.makalaster.wastelandwares.shopWares;
 
 
-import android.support.test.espresso.UiController;
-import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-import android.widget.LinearLayout;
 
 import com.makalaster.wastelandwares.R;
 import com.makalaster.wastelandwares.data.Cart;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.core.IsInstanceOf;
 import org.junit.Before;
 import org.junit.Rule;
@@ -41,7 +33,7 @@ import static org.hamcrest.Matchers.allOf;
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class CartActivityTest {
-    Cart mCart = Cart.getInstance();
+    private Cart mCart = Cart.getInstance();
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
